27206c7da45c94a6e7e8e9b3f66407a902e055b8
Juan Cremades <relicary@gmail.com>
2024-03-29 22:49:13 +0100

    Added the Superhero instance and prints the address
    Closes #1

--- typescript-intro/src/topics/nested_example.ts
+++ typescript-intro/src/topics/nested_example.ts
@@ -11,5 +11,20 @@ interface SuperHero {
     showAddress: () => string;
 }
 
+const superHero: SuperHero = {
+    name: 'Spiderman',
+    age: 30,
+    address: {
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
+    },
+    showAddress() {
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
+    }
+}
+
+const address = superHero.showAddress();
+console.log( {address} );
 
 export {};
